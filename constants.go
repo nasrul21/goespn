e8816815adf018a3314d4497f7d78d098c5bc1f3
Nasrul Faizin <faizinnasrul21@gmail.com>
2021-02-25 21:42:45 +0700

    feat(constants): add sport constants

---
+++ constants.go
@@ -0,0 +1,10 @@
+package goespn
+
+// List of Sport
+const (
+	SportSoccer     = "soccer"
+	SportFootball   = "football"
+	SportBaseball   = "baseball"
+	SportHockey     = "hockey"
+	SportBasketball = "basketball"
+)
